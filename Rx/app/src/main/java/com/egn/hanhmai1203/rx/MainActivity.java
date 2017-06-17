c4bb661178536ecfdf1a04cf6fbc58e72fc67973
Hanh Mai <hanhmai1203@Hanhs-MacBook-Pro.local>
2017-06-17 13:37:41 +0700

    first add

---
+++ Rx/app/src/main/java/com/egn/hanhmai1203/rx/MainActivity.java
@@ -0,0 +1,13 @@
+package com.egn.hanhmai1203.rx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
